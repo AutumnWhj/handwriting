9cdbf89995beb17611b82492f79abc2643815b17
wanghuajin <wanghuajin@maka.im>
2021-01-16 09:38:07 +0800

    feat:compose组合函数

---
+++ Compose/compose.js
@@ -0,0 +1,30 @@
+// 动手实现一下compose函数
+// 返回的是一个function
+// 递归实现
+function compose(...funcs) {
+  // 从args入手 从后往前执行
+  let len = funcs.length
+  let count = len - 1
+  let result = null
+  return function fn(...args) {
+    // 计算当前的结果
+    result = funcs[count].apply(this, args)
+    // 还有其他方法 则递归并把当前的result作为下一个方法的参数
+    if(count <= 0) {
+      // 重置count
+      count = len - 1
+      return result
+    } else {
+      count--
+      return fn.call(null, result)
+    }
+  }
+}
+
+
+// test
+// 测试
+const greeting = (name) => `hello ${name}`;
+const toUpper = (str) => str.toUpperCase();
+const fn = compose(toUpper, greeting);
+console.log(fn("yideng")); // 预期输出 'HELLO YIDENG'
